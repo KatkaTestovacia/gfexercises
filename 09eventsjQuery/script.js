f24251b1f1bde7d1051f610d6405fdb8876893a1
KatkaTestovacia <KatkaTestovacia@users.noreply.github.com>
2020-10-05 23:21:02 +0200

    Add events with jQuery on clicks

--- 09eventsjQuery/script.js
+++ 09eventsjQuery/script.js
@@ -2,7 +2,6 @@ $('.first-button').click(() => {
     console.log("Yeah, you clicked me")
 });
 
-
 $(".second-button").click(() => {
     console.log("you clicked the second button");
     $(".first-button").text("wrong");
@@ -12,3 +11,44 @@ $(".third-button").click(() => {
     console.log("you clicked the third button");
     $("button").css("background","red");
 });
+
+var bgcolor = "blue"
+console.log("default")
+console.log(bgcolor)
+
+function getInput() {
+    bgcolor = document.getElementById("bgcolor").value;
+    //console.log("Citam z funkcie")
+    //console.log(bgcolor)
+    submitOK = "true";
+  
+    //if (fname.length < 10) {
+    //  alert("The name may have no more than 10 characters");
+    //  submitOK = "false";
+    //} 
+  
+    if (submitOK == "false") {
+      return false;
+    }
+  }
+
+// to apply the reaction only after the FIRST click
+$(".submit").one('click',function(e) {
+    //console.log("you submitted a color");
+    $("div").text("You submitted ").append(bgcolor).append(".");
+    console.log("I read a color");
+    console.log(getInput());
+    $("button").css("background",bgcolor);
+});
+
+
+
+/*
+$(".submit").click(() => {
+    //console.log("you submitted a color");
+    $("div").text("You submitted ").append(bgcolor).append(".");
+    console.log("I read a color");
+    console.log(getInput());
+    $("button").css("background",bgcolor);
+});
+*/
